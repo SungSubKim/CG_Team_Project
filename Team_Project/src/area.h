454028673cd8d22e2de411280edd6e41edefba32
chizz <bang627@skku.edu>
2021-05-04 17:08:58 +0900

    area.h의 변경사항에 대한 주석 추가

--- Team_Project/src/area.h
+++ Team_Project/src/area.h
@@ -9,15 +9,17 @@ struct square
 	GLint z_length;
 };
 vec3 old_s_center = vec3(0);
-//장외 판정을 모든 사각형으로 나누어서한다.
+
 std::vector<struct square> divided_map2_land = {
 	{ vec3(0,0,0),36,72 },{ vec3(90,0,0),36,72 } };
+//장외 판정을 모든 사각형으로 나누어서한다.
 std::vector<struct square> divided_map2_bridge = {
 	{ vec3(36,0,9),36,9 },{ vec3(63,0,18),9,18 },
 { vec3(45,0,27),27,9 },{ vec3(45,0,36),9,18 },{ vec3(54,0,45),18,9 },
 { vec3(63,0,54),27,9 }};
-//판을 사각형으로 나누어 입력하였다.
+//판을 사각형으로 나누어 입력하였다. land는 map2의 육지 bridge는 map2의 다리
 void check_on_area() {
+	//onLand_old,now는 각각 이전 프레임에 육지위에 있었는지 아닌지를 판단,bridge 또한 같은 방법으로 작동
 	bool onLand_old = false,onLand_now = false;
 	for (auto& s : divided_map2_land) {
 		if ((s.point.x <= old_s_center.x  && old_s_center.x <= s.point.x + s.x_length
@@ -46,12 +48,16 @@ void check_on_area() {
 	}
 	printf("%f %f %f %f %d %d %d %d\n", s_center.x, s_center.z, old_s_center.x, old_s_center.z,
 		onLand_old, onLand_now, onBridge_old, onBridge_now);
-	if (onBridge_old && (!onBridge_now&&!onLand_now)) { // 다리위에서 떨어짐
-		s_center.x = 0;
-		s_center.z = 0;
-	}
-	else if (onLand_old&&(!onLand_now && !onBridge_now)) {
-		CopyMemory(s_center, old_s_center, sizeof(vec3));
+	if (!onBridge_now && !onLand_now) {
+		if (onBridge_old) {
+			// 다리위에서 떨어짐, 과거에는 다리위에 있었으나 영역밖으로 이동하려할때
+			s_center.x = 0;
+			s_center.z = 0;
+		}
+		else {
+			// 육지위에서 떨어짐, 과거에는 다리위에 있었으나 영역밖으로 이동하려할때
+			CopyMemory(s_center, old_s_center, sizeof(vec3));
+		}
 	}
 	return;
 }
