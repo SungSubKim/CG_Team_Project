d34da8cfe6192205864483fa6bb4b62a0343e733
chizz <bang627@skku.edu>
2021-05-11 23:21:42 +0900

    벽충돌 및 맵 바운더리 설정 스테이지 전환 추가

--- Team_Project/src/area.h
+++ Team_Project/src/area.h
@@ -9,7 +9,14 @@ struct square
 	GLint z_length;
 };
 vec3 old_s_center = vec3(0);
-
+std::vector<struct square> divided_map1_land = {
+	{ vec3(0,0,0),128,72 },{ vec3(90,0,0),36,72 } };
+std::vector<struct square> divided_map1_obstacle = {
+	{ vec3(0,0,0),14,16 },{ vec3(0,0,0),25,13 },{ vec3(0,0,0),30,11 },
+{ vec3(0,0,0),35,8 },{ vec3(0,0,0),38,5 },{ vec3(0,0,0),40,3 }, { vec3(0,0,38),7,72-38 }, { vec3(0,0,41),14,72-41 },
+{ vec3(0,0,45),18,72-45 }, { vec3(0,0,50),18,72-50 }, { vec3(0,0,55),20,72-55 },
+{ vec3(0,0,58),24,72-58 },{ vec3(0,0,61),29,MAP_Z-61 }, { vec3(0,0,63),33,MAP_Z-63 },
+{ vec3(0,0,65),33,MAP_Z-65 }, { vec3(0,0,69),39,MAP_Z-69 }};
 std::vector<struct square> divided_map2_land = {
 	{ vec3(0,0,0),36,72 },{ vec3(90,0,0),36,72 } };
 //장외 판정을 모든 사각형으로 나누어서한다.
@@ -22,6 +29,45 @@ std::vector<struct square> divided_map3 = {
 //장외 판정을 모든 사각형으로 나누어서한다.
 //판을 사각형으로 나누어 입력하였다. land는 map2의 육지 bridge는 map2의 다리
 
+void check_map1() {
+	//onLand_old,now는 각각 이전 프레임에 육지위에 있었는지 아닌지를 판단,bridge 또한 같은 방법으로 작동
+	model& model_character = getModel("Character");
+	vec3& s_center = model_character.center;
+	bool onLand_old = false, onLand_now = false;
+	for (auto& s : divided_map1_land) {
+		if ((s.point.x <= old_s_center.x && old_s_center.x <= s.point.x + s.x_length
+			&& s.point.z <= old_s_center.z && old_s_center.z <= s.point.z + s.z_length))
+		{
+			onLand_old = true;
+		}
+		if ((s.point.x <= s_center.x && s_center.x <= s.point.x + s.x_length
+			&& s.point.z <= s_center.z && s_center.z <= s.point.z + s.z_length))
+		{
+			onLand_now = true;
+		}
+	}
+	bool onObstacle_old = false, onObstacle_now = false;
+	for (auto& s : divided_map1_obstacle) {
+		if ((s.point.x <= old_s_center.x && old_s_center.x <= s.point.x + s.x_length
+			&& s.point.z <= old_s_center.z && old_s_center.z <= s.point.z + s.z_length))
+		{
+			onObstacle_old = true;
+		}
+		if ((s.point.x <= s_center.x && s_center.x <= s.point.x + s.x_length
+			&& s.point.z <= s_center.z && s_center.z <= s.point.z + s.z_length))
+		{
+			onObstacle_now = true;
+		}
+	}
+	if (pow((s_center.x - 69) / 25.0f, 2) + pow((s_center.z - 35) / 13.0f, 2) < 1)
+		onObstacle_now = true;
+	if (pow((old_s_center.x - 69) / 25.0f, 2) + pow((old_s_center.z - 35) / 13.0f, 2) < 1)
+		onObstacle_old = true;
+	if((!onObstacle_old && onObstacle_now)|| (onLand_old&&!onLand_now))
+		CopyMemory(s_center, old_s_center, sizeof(vec3));
+
+	return;
+}
 void check_map2() {
 	//onLand_old,now는 각각 이전 프레임에 육지위에 있었는지 아닌지를 판단,bridge 또한 같은 방법으로 작동
 	model& model_character = getModel("Character");
@@ -94,31 +140,29 @@ void check_map3() {
 inline float xz_distance(vec3 a, vec3 b) {
 	return  pow((a.x - b.x) * (a.x - b.x) + (a.z - b.z) * (a.z - b.z),0.5f);
 }
-void check_to_enemy() {
+int check_to_enemy() {
 	vec3& s_center = getModel("Character").center;
-
-	vec3& e_center1 = getModel("Enemy1").center;
-	vec3& e_center2 = getModel("Enemy2").center;
-	vec3& e_center3 = getModel("Enemy3").center;
+	int res = 0;
+	model& e1 = getModel("Enemy1"), & e2 = getModel("Enemy2"), & e3 = getModel("Enemy3");
 	/*printf("%f %f %f\n", xz_distance(e_center1, s_center), xz_distance(e_center2, s_center),
 		xz_distance(e_center3, s_center));*/
-	if (xz_distance(e_center1, s_center) < 5)
+	if (xz_distance(e1.center, s_center) < 5)
 		getModel("Enemy1").visible = false;
-	if (xz_distance(e_center2, s_center) < 5)
+	if (xz_distance(e2.center, s_center) < 5)
 		getModel("Enemy2").visible = false;
-	if (xz_distance(e_center3, s_center) < 5) {
+	if (xz_distance(e3.center, s_center) < 5) {
 		//printf("hi\n");
 		getModel("Enemy3").visible = false;
 	}
+	if (e1.visible) res++; if (e2.visible) res++; if (e3.visible) res++;
+	return res;
 }
-int getTriangle() {
+bool getTriangle() {
 	int stage=2;
+	bool res = false;
 	vec3& s_center = getModel("Character").center;
 
 	vec3& t_center = getModel("triangle").center;
-	/*printf("%f %f %f\n", xz_distance(e_center1, s_center), xz_distance(e_center2, s_center),
-		xz_distance(e_center3, s_center));*/
-	//printf("%f	%f	%f\n", s_center.x,s_center.z, xz_distance(t_center, s_center));
 	if (xz_distance(vec3(120, 0, 35), s_center) < 7) {
 		stage = 3;
 		s_center = vec3(0);
@@ -126,30 +170,42 @@ int getTriangle() {
 		getModel("Enemy3").visible = false;
 		getModel("Enemy2").visible = false;
 		getModel("Enemy1").visible = false;
+		res = true;
 	}
-	return stage;
+	return res;
 }
 void setStage(int stage) {
+	static int old_stage = 0;
 	//printf("%d\n", stage);
-	switch (stage) {
-	case 1:
-		getModel("Map1").visible = true;
-		getModel("Map2_1").visible = false;
-		getModel("Map2_3").visible = false;
-		getModel("Map3").visible = false;
-		break;
-	case 2:
-		//printf("hi");
-		getModel("Map1").visible = false;
-		getModel("Map2_1").visible = true;
-		getModel("Map2_3").visible = true;
-		getModel("Map3").visible = false;
-		break;
-	default:
-		getModel("Map1").visible = false;
-		getModel("Map2_1").visible = false;
-		getModel("Map2_3").visible = false;
-		getModel("Map3").visible = true;
-		break;
+	if (old_stage + 1 == stage) {
+		switch (stage) {
+			case 1:
+				getModel("Map1").visible = true;
+				getModel("Map2_1").visible = false;
+				getModel("Map2_3").visible = false;
+				getModel("Map3").visible = false;
+				getModel("Enemy3").visible = true;
+				getModel("Enemy2").visible = true;
+				getModel("Enemy1").visible = true;
+				break;
+			case 2:
+				//printf("hi");
+				getModel("Character").center = vec3(3, 0, 3);
+				getModel("Map1").visible = false;
+				getModel("Map2_1").visible = true;
+				getModel("Map2_3").visible = true;
+				getModel("triangle").visible = true;
+				getModel("Enemy3").visible = true;
+				getModel("Enemy2").visible = true;
+				getModel("Enemy1").visible = true;
+				break;
+			default:
+				getModel("Character").center = vec3(3, 0, 3);
+				getModel("Map2_1").visible = false;
+				getModel("Map2_3").visible = false;
+				getModel("Map3").visible = true;
+				break;
+		}
 	}
+	old_stage = stage;
 }
